aa238ff5746cb5f251ab9b5923e47356228f6757
junhanfeng <junhanfeng@gmail.com>
2016-11-09 02:26:46 +0800

    OrderStatus Done
    Haven't check on order query

--- mongodb/src/mongodb/OrderStatusTransaction.java
+++ mongodb/src/mongodb/OrderStatusTransaction.java
@@ -10,7 +10,7 @@ import com.mongodb.client.MongoCollection;
 import java.util.List;
 
 import org.bson.Document;
-import org.bson.BsonArray;
+
 
 public class OrderStatusTransaction {
 	private MongoDatabase database;
@@ -29,11 +29,11 @@ public class OrderStatusTransaction {
 /*		//@SuppressWarnings("deprecation")	
  * 		for(String s : database.listCollectionNames())
 			System.out.println("OrderStatus: "+s);*/
-		
+		// print customer
 		BasicDBObject dcCustomerQuery = new BasicDBObject().append("cWId", Integer.toString(wID)).append("cDId", Integer.toString(dID)).append("cId", Integer.toString(cID));
 		BasicDBObject dcCustomerProjection = new BasicDBObject().append("cName",1).append("cBalance", 1);
 		MongoCollection<Document> coll = database.getCollection("customer");
-		MongoCursor<Document> cursor = coll.find(dcCustomerQuery).iterator();
+		MongoCursor<Document> cursor = coll.find(dcCustomerQuery).projection(dcCustomerProjection).iterator();
 		try {
 	        while (cursor.hasNext()) {
 	        	Document dcCustomer = cursor.next();
@@ -44,7 +44,6 @@ public class OrderStatusTransaction {
 	    } finally {
 	        cursor.close();
 	    }
-		
 /*		// print customer
 		BasicDBObject dcCustomerQuery = new BasicDBObject().append("cWId", Integer.toString(wID)).append("cDId", Integer.toString(dID)).append("cId", Integer.toString(cID));
 		//BasicDBObject dcCustomerQuery = new BasicDBObject().append("cWId", wID).append("cDId", dID).append("cId", cID);
@@ -55,6 +54,44 @@ public class OrderStatusTransaction {
 		//System.out.println(String.format("Customer's Name: (%s, %s, %s) | Balance: %.2f", dcCustomer.get("cName.first").toString(), dcCustomer.get("cName.middle").toString(), dcCustomer.get("cName.last").toString(), (Double) dcCustomer.get("cBalance")));
 */
 		// get last order
+		BasicDBObject dcOrderSort = new BasicDBObject().append("oId", -1);
+		BasicDBObject dcOrderQuery = new BasicDBObject().append("oWId", wID).append("oDId", dID).append("oCId", cID);
+		BasicDBObject dcOrderProjection = new BasicDBObject().append("oId",1).append("oEntryDate", 1).append("oDeliveryDate", 1).append("oCarrierId", 1).append("oOrderLine", 1);
+		MongoCollection<Document> coll2 = database.getCollection("order");
+		MongoCursor<Document> cursor2 = coll2.find(dcOrderQuery).projection(dcOrderProjection).sort(dcOrderSort).limit(1).iterator();
+		try {
+	        while (cursor2.hasNext()) {
+	        	Document dcOrder = cursor2.next();
+	        	@SuppressWarnings("unchecked")
+	        	int oId = (Integer) dcOrder.get("oId");
+	        	System.out.println("OLD IS: "+oId);
+	        	String entryDate = dcOrder.get("oEntryDate").toString();
+	        	System.out.println("entryDate IS: "+entryDate);
+	    		int oCarrierId = (Integer) dcOrder.get("oCarrierId");
+	    		System.out.println("oCarrierId IS: "+oCarrierId);
+	    		@SuppressWarnings("unchecked")
+	    		List<Document> orderLineList = (List<Document>) dcOrder.get("oOrderLine");
+	    		System.out.println("Length IS: "+orderLineList.size());
+	    		String deliveryDate="";
+	    		try{
+	    			deliveryDate = dcOrder.get("oDeliveryDate").toString();
+	    		}catch(NullPointerException e){
+	    			deliveryDate = "Not delivered";
+	    		}
+	    		System.out.println("deliveryDate IS: "+deliveryDate);
+	    		System.out.println(String.format("Last order number: %d | Entry date & time: %s | Carrier ID: %d", oId, entryDate, oCarrierId));
+
+	    		System.out.println("Ordered Item(s)");
+	    		
+	    		for(int i=0; i < orderLineList.size(); i++){
+	    			Document orderLine = orderLineList.get(i);
+	    			System.out.println(String.format("Item No.: %d | Supplying Warehouse No.: %d | quantity: %.0f | Total price: %.2f | Delivery date & time: %s", orderLine.get("iId"), orderLine.get("supplyWId"), orderLine.get("qty"), orderLine.getDouble("amt"), deliveryDate));
+	    		}
+	        }
+	    } finally {
+	        cursor2.close();
+	    }
+		/*// get last order
 		BasicDBObject dcOrderSort = new BasicDBObject().append("oId", -1);
 		BasicDBObject dcOrderQuery = new BasicDBObject().append("oWId", wID).append("oDId", dID).append("oCId", cID);
 		BasicDBObject dcOrderProjection = new BasicDBObject().append("oId",1).append("oEntryDate", 1).append("oDeliveryDate", 1).append("oCarrierId", 1).append("oOrderLine", 1);
@@ -78,6 +115,6 @@ public class OrderStatusTransaction {
 		for(int i=0; i < orderLineList.size(); i++){
 			BasicDBObject orderLine = (BasicDBObject) orderLineList.get(i);
 			System.out.println(String.format("Item No.: %d | Supplying Warehouse No.: %d | quantity: %.0f | Total price: %.2f | Delivery date & time: %s", orderLine.getInt("iId"), orderLine.getInt("supplyWId"), orderLine.getInt("qty"), orderLine.getDouble("amt"), deliveryDate));
-		}
+		}*/
 	}
 }
